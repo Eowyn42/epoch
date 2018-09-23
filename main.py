56558b28930edce3f76d48521d92cd22ef23b443
Eowyn Baughman <eowyn42@gmail.com>
2018-09-23 08:38:45 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import time
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_epoch():
+    return time.time()
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 6738))
+    app.run(host='0.0.0.0', port=port)
